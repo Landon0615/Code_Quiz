d7f9b4b153c37157e44a497da54372f5246edf43
Landon Waddell <Landon.waddell@gmail.com>
2021-05-12 18:30:54 -0600

    Boiler Plate

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 
 
-$('#begin').on('click',function() {
-    console.log("Began");
+$('.container-fluid').hide();
+$('.container-fluid2').hide();
+$('.container-fluid3').hide();
+$('.container-fluid4').hide();
+$('.time-left').hide();
+
+function countDown() {
+    setInterval(function({
+    if(timeLeft <= 0) {
+        clearInterval
+    }
+    }))
+}
+
+
+$('button').click(function() {
+    $('button').hide();
+    $('.time-left').show();
+    $('.container-fluid').show();
+    $('.container-fluid2').hide();
+    $('.container-fluid3').hide();
+    $('.container-fluid4').hide();  
 });
+
+$('#quiz').click(function() {
+    console.log("clciked")
+    $('.container-fluid').hide();
+    $('.container-fluid2').show();
+});
+
+$('#quiz2').click(function() {
+    console.log("clciked")
+    $('.container-fluid2').hide();
+    $('.container-fluid3').show();
+});
+
+$('#quiz3').click(function() {
+    console.log("clciked")
+    $('.container-fluid3').hide();
+    $('.container-fluid4').show();
+});
+
+$('#quiz4').click(function() {
+    console.log("clciked")
+    $('.container-fluid4').hide();
+});
+
