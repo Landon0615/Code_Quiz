90868941752d40351735b020778224389a7c712a
Landon Waddell <Landon.waddell@gmail.com>
2021-05-12 22:30:10 -0600

    Added a highscore page and a timer.

--- script.js
+++ script.js
@@ -4,46 +4,63 @@ $('.container-fluid').hide();
 $('.container-fluid2').hide();
 $('.container-fluid3').hide();
 $('.container-fluid4').hide();
-$('.time-left').hide();
+$('form').hide();
+$('span').hide();
 
-function countDown() {
-    setInterval(function({
-    if(timeLeft <= 0) {
-        clearInterval
-    }
-    }))
-}
 
 
 $('button').click(function() {
     $('button').hide();
-    $('.time-left').show();
+    $('h3').show();
+    $('span').show();
     $('.container-fluid').show();
     $('.container-fluid2').hide();
     $('.container-fluid3').hide();
-    $('.container-fluid4').hide();  
+    $('.container-fluid4').hide();
 });
 
 $('#quiz').click(function() {
-    console.log("clciked")
+    console.log("clicked")
     $('.container-fluid').hide();
     $('.container-fluid2').show();
 });
 
 $('#quiz2').click(function() {
-    console.log("clciked")
+    console.log("clicked")
     $('.container-fluid2').hide();
     $('.container-fluid3').show();
 });
 
 $('#quiz3').click(function() {
-    console.log("clciked")
+    console.log("clicked")
     $('.container-fluid3').hide();
     $('.container-fluid4').show();
 });
 
 $('#quiz4').click(function() {
-    console.log("clciked")
+    console.log("clicked")
     $('.container-fluid4').hide();
+    $('form').show();
 });
+
+function c(){
+    var n=$('.c').attr('id');
+    var c=n;
+    $('.c').text(c);
+    setInterval(function(){
+        c--;
+        if(c>=0){
+            $('.c').text(c);
+        }
+        if(c==0){
+            $('.c').text(n);
+        }
+    },1000);
+}
+
+// Start
+c();
+
+// Loop
+
 
